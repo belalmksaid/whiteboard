a509c309522cd8071e036af523afa4273b7643c8
Ubuntu <ubuntu@ip-172-31-42-48.us-east-2.compute.internal>
2017-11-14 22:26:02 +0000

    Set up server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/instant', function(req, res, next) {
+});
+
+app.post('/', function(req, res, next) {
+});
+
+
+app.listen(3000, function() {
+});
