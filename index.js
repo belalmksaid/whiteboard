2ae42f9703429636c7ad763530947e6066b191a0
Ubuntu <ubuntu@ip-172-31-42-48.us-east-2.compute.internal>
2017-11-17 06:05:54 +0000

    Add history logging

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const moveTo = 0;
+const lineTo = 1;
+const clear = 2;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -7,10 +17,22 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/instant', function(req, res, next) {
+const canvaslog = new Array();
+let guid = 0;
+
+app.get('/whiteboard/all', function(req, res, next) {
+	res.send(JSON.stringify(canvaslog));
 });
 
-app.post('/', function(req, res, next) {
+app.post('/whiteboard', function(req, res, next) {
+	res.send("Hi");
+	for(let i = 0; i < req.body.length; i++) {
+		let temp = req.body[i];
+		temp.id = guid++;
+		if(temp.t == clear)
+			canvaslog.length = 0;
+		canvaslog.push(temp);
+	}
 });
 
 
